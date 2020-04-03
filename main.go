072a151b88ce09b58412d87ffdfbf124aec94f1e
Kerem Balci <kerem.balci@achievers.com>
2020-04-02 20:33:51 -0400

    init commit. Create basic http web server

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		log.Println("Hello World")
+		d, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			// rw.WriteHeader(http.StatusBadRequest)
+			// rw.Write([]byte("oops"))
+			http.Error(rw, "oops", http.StatusBadRequest)
+			return
+		}
+
+		// log.Printf("Data %s", d)
+
+		fmt.Fprintf(rw, "Hello %s", d)
+	})
+
+	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
+		log.Println("Goodbye World")
+	})
+
+	http.ListenAndServe(":9090", nil)
+}
