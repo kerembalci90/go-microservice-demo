c48581095bb0a3d9c401c83f64d10548cf9d26ac
Kerem BALCI <kerembalci90@gmail.com>
2020-04-03 23:05:52 -0400

    post method implementation to create new products

--- handlers/products.go
+++ handlers/products.go
@@ -22,14 +22,32 @@ func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	if r.Method == http.MethodPost {
+		p.CreateProduct(rw, r)
+		return
+	}
+
 	rw.WriteHeader(http.StatusMethodNotAllowed)
 }
 
 func (p *Products) GetProducts(rw http.ResponseWriter, r *http.Request) {
+	p.log.Println("Handle GET Products request")
 	lp := data.GetProducts()
 	err := lp.ToJSON(rw)
 	// d, err := json.Marshal(lp)
 	if err != nil {
 		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
 	}
+}
+
+func (p *Products) CreateProduct(rw http.ResponseWriter, r *http.Request) {
+	p.log.Println("Handle POST Product request")
+	product := &data.Product{}
+	err := product.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to unmarshall JSON", http.StatusBadRequest)
+	}
+
+	p.log.Printf("Prod: %#v", product)
+	data.AddProduct(product)
 }
